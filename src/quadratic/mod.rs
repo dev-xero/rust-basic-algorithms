d4150825925b3d06155c0f22387a848ce8abe578
Xero kun <elo48858@gmail.com>
2023-10-01 15:42:15 +0100

    feat: Improved code structure

--- src/quadratic/mod.rs
+++ src/quadratic/mod.rs
@@ -1,5 +1,3 @@
-use std::{process::exit, num::ParseIntError};
-
 #[derive(Debug, PartialEq)]
 pub struct Quadratic {
     pub equation: String,  // ax^2 + bx + c
@@ -7,50 +5,35 @@ pub struct Quadratic {
 }
 
 impl Quadratic {
-    fn parse_term(term: &str) -> Result<i32, ParseIntError> {
-        if term[..].contains("-") {
-            return Ok(term[..2].parse::<i32>()?)
-        } else if term[..].contains("+") {
-            return Ok(term[1..2].parse::<i32>()?)
+    fn parse_term(term: &str) -> Result<i32, &'static str> {
+        if term.contains("-") {
+            Ok(term[..2].parse::<i32>().map_err(|_| "Failed to parse term")?)
+        } else if term.contains("+") {
+            Ok(term[1..2].parse::<i32>().map_err(|_| "Failed to parse term")?)
         } else {
-            return Ok(term[..1].parse::<i32>()?);
+            Ok(term[..1].parse::<i32>().map_err(|_| "Failed to parse term")?)
         }
     }
 
-    pub fn from_equation(equation: &str) -> Result<Quadratic, ParseIntError> {
-        let components: Vec<&str> = equation.split(" ").collect();
+    pub fn from_equation(equation: &str) -> Result<Quadratic, &'static str> {
+        let components: Vec<&str> = equation.split_whitespace().collect();
 
         if components.len() < 3 {
-            eprintln!("Not enough terms for quadratic equation");
-            exit(1);
+            return Err("Not enough arguments")
         }
 
-        let a_string = components.get(0)
-            .unwrap_or_else(|| {
-                eprintln!("Constant term 'a' is missing.");
-                exit(1);
-            });
-        
-        let b_string = components.get(1)
-            .unwrap_or_else(|| {
-                eprintln!("Constant term 'b' is missing.");
-                exit(1);
-            });
-        
-        let c_string = components.get(2)
-            .unwrap_or_else(|| {
-                eprintln!("Constant term 'c' is missing.");
-                exit(1);
-            });
+        let a_term: &str = components.get(0).ok_or_else(|| "Constant term 'a' is missing.")?;
+        let b_term = components.get(1).ok_or_else(|| "Constant term 'b' is missing.")?;
+        let c_term = components.get(2).ok_or_else(|| "Constant term 'c' is missing.")?;
 
-        let a: i32 =  if a_string
+        let a: i32 =  if a_term
             .chars().nth(0).is_some_and(|t| t.is_alphabetic()) {
             1
         } else {
-            Self::parse_term(&a_string)?
+            Self::parse_term(&a_term)?
         };
-        let b: i32 = Self::parse_term(&b_string)?;
-        let c: i32 = Self::parse_term(&c_string)?;
+        let b: i32 = Self::parse_term(&b_term)?;
+        let c: i32 = Self::parse_term(&c_term)?;
 
         Ok(
             Quadratic { 
