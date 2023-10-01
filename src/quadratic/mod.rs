c157c3b736cec941b465679dca2dedea82d7a60e
Xero kun <elo48858@gmail.com>
2023-10-01 14:29:13 +0100

    fix(bug): Fixed bug in constant term parsing

--- src/quadratic/mod.rs
+++ src/quadratic/mod.rs
@@ -7,45 +7,59 @@ pub struct Quadratic {
 }
 
 impl Quadratic {
+    fn parse_term(term: &str) -> i32 {
+        return if term[..].contains("-") {
+            term[..2].parse::<i32>().unwrap_or_else(|err| {
+                eprintln!("{}", format!("Failed to parse the constant term : {}", err));
+                exit(1);
+            })
+        } else if term[..].contains("+") {
+            term[1..2].parse::<i32>().unwrap_or_else(|err| {
+                eprintln!("{}", format!("Failed to parse the constant term: {}", err));
+                exit(1);
+            })
+        } else {
+            term[..1].parse::<i32>().unwrap_or_else(|err| {
+                eprintln!("{}", format!("Failed to parse the constant term: {}", err));
+                exit(1);
+            })
+        };
+    }
+
     pub fn from_equation(equation: &str) -> Quadratic {
         let components: Vec<&str> = equation.split(" ").collect();
-        if components.len() != 5 {
+
+        if components.len() < 3 {
             eprintln!("Not enough terms for quadratic equation");
             exit(1);
         }
 
-        let a: i32 = components.get(0)
+        let a_string = components.get(0)
             .unwrap_or_else(|| {
                 eprintln!("Constant term 'a' is missing.");
                 exit(1);
-            })
-            .parse()
-            .unwrap_or_else(|err| {
-                eprintln!("{}", format!("Failed to parse the constant term 'a': {}", err));
-                exit(1);
             });
 
-        let b: i32 = components.get(2)
+        if a_string.chars().nth(0).is_some_and(|t| t.is_alphabetic()) {
+            eprintln!("Invalid 'a' term");
+            exit(1);
+        }
+        
+        let b_string = components.get(1)
             .unwrap_or_else(|| {
                 eprintln!("Constant term 'b' is missing.");
                 exit(1);
-            })
-            .parse()
-            .unwrap_or_else(|err| {
-                eprintln!("{}", format!("Failed to parse the constant term 'b': {}", err));
-                exit(1);
             });
-
-        let c: i32 = components.get(4)
+        
+        let c_string = components.get(2)
             .unwrap_or_else(|| {
                 eprintln!("Constant term 'c' is missing.");
                 exit(1);
-            })
-            .parse()
-            .unwrap_or_else(|err| {
-                eprintln!("{}", format!("Failed to parse the constant term 'c': {}", err));
-                exit(1);
             });
+        
+        let a: i32 = Self::parse_term(&a_string);
+        let b: i32 = Self::parse_term(&b_string);
+        let c: i32 = Self::parse_term(&c_string);
 
         Quadratic { 
             equation: equation.clone().to_string(), 
--- tests/test_quadratic.rs
+++ tests/test_quadratic.rs
@@ -2,7 +2,8 @@ use rust_basic_algorithms::Quadratic;
 
 #[test]
 fn test_from_equation() {
-    let equation = String::from("4x2 + 2x + 8");
+    let equation = String::from("4x2 +2x +8");
+
     let result = Quadratic::from_equation(&equation);
     let expected = Quadratic {
         equation,
@@ -10,4 +11,25 @@ fn test_from_equation() {
     };
 
     assert_eq!(result, expected, "Quadratic equation should be built from the equation string");
+}
+
+#[test]
+fn test_from_equation_with_negatives() {
+    let equation = String::from("-4x2 -2x +8");
+
+    let result = Quadratic::from_equation(&equation);
+    let expected = Quadratic {
+        equation,
+        variables: (-4, -2, 8)
+    };
+
+    assert_eq!(result, expected, "Quadratic equation should be built from the equation string");
+}
+
+#[test]
+#[should_panic]
+fn test_from_equation_with_invalid_eq() {
+    let equation = String::from("x2 +2x 8x2");
+
+    Quadratic::from_equation(&equation);
 }
