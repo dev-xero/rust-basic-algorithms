990f45a3fa5596bd8c9e4ad39248b61066ff13ec
Xero kun <elo48858@gmail.com>
2023-10-01 15:02:02 +0100

    test: Added more tests for parsing constant terms

--- tests/test_quadratic.rs
+++ tests/test_quadratic.rs
@@ -4,7 +4,7 @@ use rust_basic_algorithms::Quadratic;
 fn test_from_equation() {
     let equation = String::from("4x2 +2x +8");
 
-    let result = Quadratic::from_equation(&equation);
+    let result = Quadratic::from_equation(&equation).unwrap();
     let expected = Quadratic {
         equation,
         variables: (4, 2, 8)
@@ -17,7 +17,7 @@ fn test_from_equation() {
 fn test_from_equation_with_negatives() {
     let equation = String::from("-4x2 -2x +8");
 
-    let result = Quadratic::from_equation(&equation);
+    let result = Quadratic::from_equation(&equation).unwrap();
     let expected = Quadratic {
         equation,
         variables: (-4, -2, 8)
@@ -27,9 +27,22 @@ fn test_from_equation_with_negatives() {
 }
 
 #[test]
-#[should_panic]
+fn test_from_equation_with_omitted_a_term() {
+    let equation = String::from("x2 +2x -8x2");
+    let result = Quadratic::from_equation(&equation).unwrap();
+    let expected = Quadratic {
+        equation,
+        variables: (1, 2, -8)
+    };
+
+    assert_eq!(result, expected, "Quadratic equation should be built from the equation string")
+}
+
+#[test]
 fn test_from_equation_with_invalid_eq() {
     let equation = String::from("x2 +2x 8x2");
 
-    Quadratic::from_equation(&equation);
+    Quadratic::from_equation(&equation).unwrap_or_else(|err| {
+    panic!("{}", err);
+    });
 }
