7d7cac0362607bcb16ea5ad00a3768f721eb1032
Xero kun <elo48858@gmail.com>
2023-10-02 17:14:58 +0100

    feat: Implemented algorithm for checking primes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 pub mod quadratic;
 pub mod fact;
 pub mod fib;
+pub mod primes;
 
 pub use quadratic::Quadratic;
 pub use fact::fact;
+pub use primes::is_prime;
---
+++ src/primes/mod.rs
@@ -0,0 +1,24 @@
+pub fn is_prime(n: u32) -> bool {
+    if n <= 1 {
+        return false
+    }
+
+    if n <= 3 {
+        return true
+    }
+
+    if n % 2 == 0 || n % 3 == 0 {
+        return false
+    }
+
+    let mut i = 5;
+
+    while i*i <= n {
+        if n % i == 0 || n % (i + 2) == 0 {
+            return false
+        }
+        i += 6
+    }
+
+    true
+}
